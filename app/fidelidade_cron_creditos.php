f4967a8876b0c4dc2c1ef9d30b042b5bbf7b41e0
Rafael Luiz <rafael@agenciaclose.com.br>
2026-06-03 20:43:54 -0300

    SISTEMA SIS

--- app/fidelidade_cron_creditos.php
+++ app/fidelidade_cron_creditos.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Cron: credita pontos de fidelidade em reservas pagas que ainda não têm movimento `credito_reserva_app`.
- *
- * Chamada (GET, recomendado para agendador):
- *   GET .../app/fidelidade_cron_creditos.php?token=SEU_TOKEN
- *
- * Ou POST JSON: { "token": "SEU_TOKEN" } (mesmo TOKEN da API em config.php)
- */
 include('../config/config.php');
 
 header('Content-Type: application/json; charset=utf-8');
